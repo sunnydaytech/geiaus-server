41b7274bf35b98ab1d07f5b23ac846eaae60670f
Nicholas <nicholas.zhaoyu@gmail.com>
2016-04-16 17:49:54 -0700

    Hello gRPC service

---
+++ service/client/client.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	pb "geia.us/service/proto"
+	"golang.org/x/net/context"
+	"google.golang.org/grpc"
+	"log"
+)
+
+const (
+	address = "localhost:50051"
+)
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer conn.Close()
+	c := pb.NewHelloWorldClient(conn)
+
+	r, err := c.Print(context.Background(), &pb.Request{})
+	if err != nil {
+		log.Fatalf("Could not print: %v", err)
+	}
+	log.Printf(r.Context)
+}
