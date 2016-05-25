61fc76c3e60dd68a4490bdee5d99e8f4074dfc5e
Nicholas <nicholas.zhaoyu@gmail.com>
2016-05-24 22:46:28 -0700

    rm web directory

--- web/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var app = express();
-
-app.use('/static', express.static(__dirname + '/public'));
-app.get('/', function(req, res){
-  res.send('hello world');
-});
-
-app.listen(3000);
