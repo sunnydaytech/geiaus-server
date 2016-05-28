6455c40c8d336f741b4efbc4acbd909b35212846
Nicholas <nicholas.zhaoyu@gmail.com>
2016-05-27 22:14:58 -0700

    Fix genrpc.sh

--- build/genrpc.sh
+++ build/genrpc.sh
@@ -1,7 +1,7 @@
 protoc=".bin/protoc"
 
 # download protoc binary.
-if [ -b "$protoc" ]
+if [ ! -f "$protoc" ]
 then
   wget -O .download/protoc.zip https://github.com/google/protobuf/releases/download/v3.0.0-beta-2/protoc-3.0.0-beta-2-osx-x86_64.zip
   unzip .download/protoc.zip -d .bin
